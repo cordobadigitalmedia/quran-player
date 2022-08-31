80bf791e42a83ac6c0b27c88a2c6270238d0d33d
Ismael Rumzan <ismael@cordobadigitalmedia.com>
2022-08-31 07:35:40 +0300

    color and play behavior

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from "next/head";
 import React, { useState, useEffect, useRef } from "react";
+import {
+  ArrowLeftCircleIcon,
+  ArrowRightCircleIcon,
+} from "@heroicons/react/24/outline";
 import AudioPlayer from "../src/components/AudioPlayer";
 import useSWR from "swr";
 
-const pageRoot = "https://quran-images-api.herokuapp.com/show/page/";
 const quranAPI =
   "https://api.quran.com/api/v4/quran/verses/uthmani?page_number=";
 
@@ -14,6 +17,7 @@ export default function Home() {
   const { data, error } = useSWR(`${quranAPI}${currentPage}`, fetcher);
   const [currentSrc, setCurrentSrc] = useState();
   const [currentId, setCurrentId] = useState("7_1");
+  const [playStatus, setPlayStatus] = useState(true);
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
 
@@ -26,8 +30,11 @@ export default function Home() {
       src = `00${idParts[0]}${idParts[1]}.mp3`;
     }
     setCurrentId(id);
-    setCurrentSrc(src);
-    console.log(src);
+    if (src === currentSrc) {
+      setPlayStatus(!playStatus);
+    } else {
+      setCurrentSrc(src);
+    }
   };
 
   const findVerseStart = (verses) => {
@@ -37,13 +44,17 @@ export default function Home() {
     return startVerse;
   };
 
+  const resetSrc = (src) => {
+    setCurrentSrc(src);
+  };
+
   return (
     <>
       <Head>
         <title>Quran Recitation</title>
       </Head>
       <div className="flex flex-col bg-[#E9D8BE]">
-        <div className="flex justify-center justify-items-stretch p-2 bg-[#120000] text-white fixed top-0 left-0 right-0 drop-shadow-md">
+        <div className="flex justify-center justify-items-stretch p-2 bg-[#293158] text-white fixed top-0 left-0 right-0 drop-shadow-md">
           <div className="w-full lg:w-[1024px] flex">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -62,36 +73,18 @@ export default function Home() {
             <div className="pl-2 flex flex-grow justify-center text-lg md:text-xl lg:text-2xl truncate">
               تلاوة القرآن بصوت عبدالله عادل
             </div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
+            <ArrowRightCircleIcon
               className="h-8 w-8 cursor-pointer"
-              onClick={() => setCurrentPage(currentPage - 1)}
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
+              onClick={() => {
+                setCurrentPage(currentPage - 1);
+              }}
+            />
+            <ArrowLeftCircleIcon
               className="h-8 w-8 cursor-pointer"
-              onClick={() => setCurrentPage(currentPage + 1)}
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"
-              />
-            </svg>
+              onClick={() => {
+                setCurrentPage(currentPage + 1);
+              }}
+            />
           </div>
         </div>
         <div
@@ -109,10 +102,10 @@ export default function Home() {
                         currentId === verse.verse_key
                           ? `border-2 bg-white`
                           : `border-0`
-                      } border-indigo-600 rounded-lg p-2 hover:bg-slate-200 flex`}
+                      } border-[#293158] rounded-lg p-2 hover:bg-slate-200 flex`}
                       onClick={(e) => playVerse(verse.verse_key)}
                     >
-                      <span className="bg-indigo-600 text-white rounded-full pt-2 pb-1 ml-2 text-base flex items-center self-start mt-2 justify-center h-7 max-w-[30px] w-full">
+                      <span className="bg-[#293158] text-white rounded-full pt-2 pb-1 ml-2 text-base flex items-center self-start mt-2 justify-center h-7 max-w-[30px] w-full">
                         {verse.verse_key.split(":")[1]}
                       </span>
                       <span className="text-3xl leading-relaxed">
@@ -125,13 +118,17 @@ export default function Home() {
             )}
           </div>
         </div>
-        <div className="flex justify-center p-2 fixed bottom-12 left-0 right-0 bg-[#E9D8BE] text-xl drop-shadow-[0_-2px_5px_rgba(0,0,0,0.15)]">
-          {`الأعراف | صفحة ${currentPage}`}
-        </div>
-        <div className="flex justify-center p-2 bg-[#120000] fixed bottom-0 left-0 right-0">
+        <div className="flex justify-center p-2 bg-[#293158] fixed bottom-8 left-0 right-0">
           <div className="w-full lg:w-[1024px] flex">
-            <AudioPlayer audioSrc={currentSrc} />
+            <AudioPlayer
+              audioSrc={currentSrc}
+              onPlayEnd={resetSrc}
+              playStatus={playStatus}
+            />
           </div>
+        </div>
+        <div className="flex justify-center p-1 text-gray-100 fixed bottom-0 left-0 right-0 bg-[#293158] text-l">
+          {`الأعراف | صفحة ${currentPage}`}
         </div>
       </div>
     </>
--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect, useRef } from "react";
+import { PlayIcon, PauseIcon } from "@heroicons/react/24/solid";
 
 const audioRoot = "https://quran-adel.s3.eu-central-1.amazonaws.com/";
 
-export default function AudioPlayer({ audioSrc = "" }) {
+export default function AudioPlayer({ audioSrc, playStatus, onPlayEnd }) {
   const [isPlaying, setIsPlaying] = useState(false);
   const [trackProgress, setTrackProgress] = useState(0);
   const audioRef = useRef(new Audio(`${audioRoot}${audioSrc}`));
@@ -13,30 +14,32 @@ export default function AudioPlayer({ audioSrc = "" }) {
 
   useEffect(() => {
     if (isPlaying) {
-      audioRef?.current.play();
-      startTimer();
+      if (audioSrc !== "") {
+        audioRef?.current.play();
+        startTimer();
+      }
     } else {
       clearInterval(intervalRef.current);
-      if (audioSrc !== "") {
+      if (audioSrc !== "" || playStatus === false) {
         audioRef?.current.pause();
       }
     }
-  }, [isPlaying, audioSrc]);
+  }, [isPlaying, audioSrc, playStatus]);
 
   useEffect(() => {
     audioRef.current.pause();
     if (audioSrc !== "") {
       audioRef.current = new Audio(`${audioRoot}${audioSrc}`);
+      if (isReady.current) {
+        audioRef.current.play();
+        setIsPlaying(true);
+        startTimer();
+      } else {
+        // Set the isReady ref as true for the next pass
+        isReady.current = true;
+      }
     }
-    if (isReady.current) {
-      audioRef.current.play();
-      setIsPlaying(true);
-      startTimer();
-    } else {
-      // Set the isReady ref as true for the next pass
-      isReady.current = true;
-    }
-  }, [audioSrc]);
+  }, [audioSrc, playStatus]);
 
   useEffect(() => {
     // Pause and clean up on unmount
@@ -52,6 +55,7 @@ export default function AudioPlayer({ audioSrc = "" }) {
       if (audioRef.current.ended) {
         setIsPlaying(false);
         setTrackProgress(0);
+        onPlayEnd("");
       } else {
         setTrackProgress(audioRef.current.currentTime);
       }
@@ -84,33 +88,14 @@ export default function AudioPlayer({ audioSrc = "" }) {
     return v < 10 ? "0" + v : v;
   };
   return (
-    <div className="flex w-full text-gray-10 h-8">
+    <div className="flex w-full text-gray-10 h-6">
       {isPlaying ? (
         <div onClick={() => setIsPlaying(false)}>
-          <svg
-            width="21"
-            height="26"
-            className="h-7 w-7 cursor-pointer"
-            viewBox="0 0 21 26"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M3.81333 0C4.82469 0 5.79463 0.456546 6.50977 1.2692C7.22491 2.08186 7.62667 3.18406 7.62667 4.33333V21.6667C7.62667 22.8159 7.22491 23.9181 6.50977 24.7308C5.79463 25.5435 4.82469 26 3.81333 26C2.80197 26 1.83204 25.5435 1.1169 24.7308C0.40176 23.9181 2.13128e-08 22.8159 0 21.6667V4.33333C0 3.18406 0.40176 2.08186 1.1169 1.2692C1.83204 0.456546 2.80197 0 3.81333 0V0ZM16.5244 0C17.5358 0 18.5057 0.456546 19.2209 1.2692C19.936 2.08186 20.3378 3.18406 20.3378 4.33333V21.6667C20.3378 22.8159 19.936 23.9181 19.2209 24.7308C18.5057 25.5435 17.5358 26 16.5244 26C15.5131 26 14.5431 25.5435 13.828 24.7308C13.1129 23.9181 12.7111 22.8159 12.7111 21.6667V4.33333C12.7111 3.18406 13.1129 2.08186 13.828 1.2692C14.5431 0.456546 15.5131 0 16.5244 0Z"
-              fill="#E2E2E2"
-            />
-          </svg>
+          <PauseIcon className="h-7 w-7 cursor-pointer text-white rtl rotate-180" />
         </div>
       ) : (
         <div onClick={() => setIsPlaying(true)}>
-          <svg
-            className="h-7 w-7 cursor-pointer"
-            viewBox="0 0 21 26"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M21 13L0 25.1244V0.875645L21 13Z" fill="#E2E2E2" />
-          </svg>
+          <PlayIcon className="h-7 w-7 cursor-pointer text-white rtl rotate-180" />
         </div>
       )}
       <div className="text-gray-100 w-16 px-2 pt-1 text-sm">{`${sToTime(
