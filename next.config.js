b44095ef640a40c5d629a49e09add202121c71d7
Ismael Rumzan <ismael@cordobadigitalmedia.com>
2022-09-19 08:33:07 +0300

    detail page

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    images: {
-      allowFutureImage: true,
-    },
-  },
-  images: {
-    domains: ["res.cloudinary.com"],
-  },
 };
 
 module.exports = nextConfig;
